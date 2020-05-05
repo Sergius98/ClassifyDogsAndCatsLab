93824207e461fb878d9f5584195815ca478f6f55
sergius98 <sergei.mykytenko@gmail.com>
2020-05-05 14:16:53 +0300

    init preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,8 @@
+# save pre-processed images into standard directories (slower, but can run on most PC)
+def to_folder():
+    pass
+
+# save pre-processed images into a single NumPy array (faster, but require a lot of RAM)
+def to_array():
+    pass
+
