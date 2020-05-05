c56b2d6dc3b93d0e7b1e5545aa7d861233f72932
sergius98 <sergei.mykytenko@gmail.com>
2020-05-05 18:16:04 +0300

    reformatted preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,26 +1,22 @@
 from os import listdir
 from os import makedirs
-from numpy import asarray
-from numpy import save
 from shutil import copyfile
-from random import seed
-from random import random
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
+
 
 # save final pre-processed images into standard directories (slower, but can run on most PC)
 def to_final_folder(src_directory='train/', dataset_home='finalize_dogs_vs_cats/',
-              cat_tag='cat', dog_tag='dog', labeldirs=['dogs/', 'cats/']):
+                    cat_tag='cat', dog_tag='dog'):
+    labeldirs = ['dogs/', 'cats/']
     for labldir in labeldirs:
         newdir = dataset_home + labldir
         makedirs(newdir, exist_ok=True)
     for file in listdir(src_directory):
         src = src_directory + '/' + file
         if file.startswith(cat_tag):
-            dst = dataset_home + 'cats/' + file
+            dst = dataset_home + labeldirs[1] + file
             copyfile(src, dst)
         elif file.startswith(dog_tag):
-            dst = dataset_home + 'dogs/' + file
+            dst = dataset_home + labeldirs[0] + file
             copyfile(src, dst)
 
 
