cec7f46d4f483f0cc8cb7eb22fb0f078a2e35110
sergius98 <sergei.mykytenko@gmail.com>
2020-05-05 17:47:57 +0300

    loaded image in load_image(filename)

--- predict.py
+++ predict.py
@@ -1,6 +1,21 @@
+# make a prediction for a new image.
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+
+
 # load and prepare the image
 def load_image(filename):
-    pass
+    # load the image
+    img = load_img(filename, target_size=(224, 224))
+    # convert to array
+    img = img_to_array(img)
+    # reshape into a single sample with 3 channels
+    img = img.reshape(1, 224, 224, 3)
+    # center pixel data
+    img = img.astype('float32')
+    img = img - [123.68, 116.779, 103.939]
+    return img
 
 
 # load an image and predict the class
