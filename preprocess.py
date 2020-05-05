9237b764a925ad811a1dcea2910f9f7dc6c61eed
ZdyrkoVlad <31853587+ZdyrkoVlad@users.noreply.github.com>
2020-05-05 17:47:00 +0300

    save pre-processed images

--- preprocess.py
+++ preprocess.py
@@ -24,32 +24,6 @@ def to_final_folder(src_directory='train/', dataset_home='finalize_dogs_vs_cats/
             dst = dataset_home + 'dogs/' + file
             copyfile(src, dst)
 
-# save pre-processed images into standard directories (slower, but can run on most PC)
-def to_folder(src_directory='train/', dataset_home='dataset_dogs_vs_cats/',
-              cat_tag='cat', dog_tag='dog',
-              subdirs=['train/', 'test/'], labeldirs=['dogs/', 'cats/']):
-    # create directories
-    for subdir in subdirs:
-        # create label subdirectories
-        for labldir in labeldirs:
-            newdir = dataset_home + subdir + labldir
-            makedirs(newdir, exist_ok=True)
-    # seed random number generator
-    seed(1)
-    # define ratio of pictures to use for validation
-    val_ratio = 0.25
-    # copy training dataset images into subdirectories
-    for file in listdir(src_directory):
-        src = src_directory + '/' + file
-        dst_dir = 'train/'
-        if random() < val_ratio:
-            dst_dir = 'test/'
-        if file.startswith(cat_tag):
-            dst = dataset_home + dst_dir + cat_tag + 's/' + file
-            copyfile(src, dst)
-        elif file.startswith(dog_tag):
-            dst = dataset_home + dst_dir + dog_tag + 's/' + file
-            copyfile(src, dst)
 
 
 # save pre-processed images into a single NumPy array (faster, but require a lot of RAM)
@@ -83,4 +57,4 @@ def to_array(folder='train', cat_tag='cat', dog_tag='dog'):
     save('dogs_vs_cats_labels.npy', labels)
 
 
-to_folder()
+to_final_folder()
