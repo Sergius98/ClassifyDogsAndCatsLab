bd06530261d885935b6cf182023e29786bb90a33
sergius98 <sergei.mykytenko@gmail.com>
2020-05-05 18:13:33 +0300

    removed subdirs from to_final_folder

--- preprocess.py
+++ preprocess.py
@@ -10,8 +10,7 @@ from keras.preprocessing.image import img_to_array
 
 # save final pre-processed images into standard directories (slower, but can run on most PC)
 def to_final_folder(src_directory='train/', dataset_home='finalize_dogs_vs_cats/',
-              cat_tag='cat', dog_tag='dog',
-              subdirs=['train/', 'test/'], labeldirs=['dogs/', 'cats/']):
+              cat_tag='cat', dog_tag='dog', labeldirs=['dogs/', 'cats/']):
     for labldir in labeldirs:
         newdir = dataset_home + labldir
         makedirs(newdir, exist_ok=True)
