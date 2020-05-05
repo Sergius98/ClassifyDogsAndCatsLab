77e5d9befb77c59b47ab3006f47cd8e1a98bf2e5
sergius98 <sergei.mykytenko@gmail.com>
2020-05-05 14:23:28 +0300

    init model.py

---
+++ model.py
@@ -0,0 +1,14 @@
+# define cnn model
+def define_model():
+    pass
+
+
+# plot diagnostic learning curves
+def summarize_diagnostics(history):
+    pass
+
+
+# run the test harness for evaluating a model
+def run_test_harness():
+    pass
+
