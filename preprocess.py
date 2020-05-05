d6d83ae58f7cfad737a1027856c1d0f17ebb211f
sergius98 <sergei.mykytenko@gmail.com>
2020-05-05 17:59:04 +0300

    deleted to numPy Array function as it is no longer required

--- preprocess.py
+++ preprocess.py
@@ -25,36 +25,4 @@ def to_final_folder(src_directory='train/', dataset_home='finalize_dogs_vs_cats/
             copyfile(src, dst)
 
 
-
-# save pre-processed images into a single NumPy array (faster, but require a lot of RAM)
-def to_array(folder='train', cat_tag='cat', dog_tag='dog'):
-    # load dogs vs cats dataset, reshape and save to a new file
-    # define location of dataset
-    folder += '/'
-    photos, labels = list(), list()
-    # enumerate files in the directory
-    for file in listdir(folder):
-        # determine class
-        if file.startswith(dog_tag):
-            output = 0.0
-        elif file.startswith('cat'):
-            output = 1.0
-        else:
-            raise Exception("found a file that belongs to neither classes : " + file)
-        # load image
-        photo = load_img(folder + file, target_size=(200, 200))
-        # convert to numpy array
-        photo = img_to_array(photo)
-        # store
-        photos.append(photo)
-        labels.append(output)
-    # convert to a numpy arrays
-    photos = asarray(photos)
-    labels = asarray(labels)
-    print(photos.shape, labels.shape)
-    # save the reshaped photos
-    save('dogs_vs_cats_photos.npy', photos)
-    save('dogs_vs_cats_labels.npy', labels)
-
-
 to_final_folder()
