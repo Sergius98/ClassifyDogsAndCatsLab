9be5f64971ed89b9e9146c7f6c6d8f2f714993cb
ZdyrkoVlad <31853587+ZdyrkoVlad@users.noreply.github.com>
2020-05-05 17:04:14 +0300

    Dropout Regularization
    add Dropout

--- model.py
+++ model.py
@@ -15,15 +15,19 @@ from keras.preprocessing.image import ImageDataGenerator
 
 # define cnn model
 def define_model():
-   	model = Sequential()
+	model = Sequential()
 	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(200, 200, 3)))
 	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.2))
 	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
 	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.2))
 	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
 	model.add(MaxPooling2D((2, 2)))
+	model.add(Dropout(0.2))
 	model.add(Flatten())
 	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dropout(0.5))
 	model.add(Dense(1, activation='sigmoid'))
 	# compile model
 	opt = SGD(lr=0.001, momentum=0.9)
