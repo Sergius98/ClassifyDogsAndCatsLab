541240e66c4e9895ceeec4b24c3a74e10a262107
sergius98 <sergei.mykytenko@gmail.com>
2020-05-05 14:45:27 +0300

    ploting cats and dogs images

--- view.py
+++ view.py
@@ -1,4 +1,29 @@
+# plot dog photos from the dogs vs cats dataset
+from matplotlib import pyplot
+from matplotlib.image import imread
+
+
 # plot images from dataset
-def plot_images():
+def plot_images(folder, prefix):
+    # plot first few images
+    for i in range(9):
+        # define subplot
+        pyplot.subplot(330 + 1 + i)
+        # define filename
+        filename = folder + '/' + prefix + '.' + str(i) + '.jpg'
+        # load image pixels
+        image = imread(filename)
+        # plot raw pixel data
+        pyplot.imshow(image)
+    # show the figure
+    pyplot.show()
     pass
 
+
+# define location of dataset
+folder = 'train'
+prefix_cat = 'cat'
+prefix_dog = 'dog'
+
+plot_images(folder, prefix_cat)
+plot_images(folder, prefix_dog)
