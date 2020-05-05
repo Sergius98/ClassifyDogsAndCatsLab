98ce524db69ff04c2ce93bf76f36de67fef00114
ZdyrkoVlad <31853587+ZdyrkoVlad@users.noreply.github.com>
2020-05-05 15:03:27 +0300

    add function to_array
    save pre-processed images into a single NumPy array (faster, but require a lot of RAM)

--- preprocess.py
+++ preprocess.py
@@ -39,5 +39,28 @@ def to_folder():
 
 # save pre-processed images into a single NumPy array (faster, but require a lot of RAM)
 def to_array():
-    pass
+    # load dogs vs cats dataset, reshape and save to a new file
+    # define location of dataset
+    folder = 'train/'
+    photos, labels = list(), list()
+    # enumerate files in the directory
+    for file in listdir(folder):
+        # determine class
+        output = 0.0
+        if file.startswith('cat'):
+            output = 1.0
+        # load image
+        photo = load_img(folder + file, target_size=(200, 200))
+        # convert to numpy array
+        photo = img_to_array(photo)
+        # store
+        photos.append(photo)
+        labels.append(output)
+    # convert to a numpy arrays
+    photos = asarray(photos)
+    labels = asarray(labels)
+    print(photos.shape, labels.shape)
+    # save the reshaped photos
+    save('dogs_vs_cats_photos.npy', photos)
+    save('dogs_vs_cats_labels.npy', labels)
 
